2e28963b23feabddc741dc7f66d3977d55616328
Jo <joembedded@gmail.com>
2023-12-15 14:27:15 +0100

    V0.95

--- README.md
+++ README.md
@@ -6,14 +6,33 @@ Sometimes IoT devices use UDP. However, complete UDP Servers are complex.
 But mapping UDP to e.g. PHP and back is a very simple solution.
 
 This small UDP Server in C sends all incomming UDP via Curl/libcurl 
-to a Standard Server (e.g PHP) and the result back via UDP.
+to a Standard Server (e.g PHP) and the result back via UDP. 
+Developed for use with LTX-Microcloud.
 
 For simplicity, the complete server is in one single C Source ;-)
 
 Compile it on the Server and run it as Service.
 
+![Overview](./docu/ovinfo.png "Overview")
+
+## A Minimum Script in PHP
+...
+	<?php
+		header('Content-Type: text/plain');
+		$hexplbe = @$_REQUEST['p'];
+		if (!isset($hexplbe)) die("#ERROR: No Payload");
+		$paybytes = @unpack('C*', hex2bin($hexplbe));
+		$paycount = count($paybytes);
+		if(!$paycount) die("#ERROR: Payload Format ('$hexplbe')");
+		$payrep =  bin2hex(pack("CN", $paycount,time()));
+		echo $payrep; // Output: Cnt.u8 Time.u32
+	?>
+...
+
 ## Installation/Test ##
 - Tested with Visual Studio (Windows) and GCC
+- For details see Sourcecode
+- Here is HowTo install a System Service on UBUNTU Linux: *todo*
 
 ## Links ##
 - Curl/libcurl: https://curl.se
--- udpsg.c
+++ udpsg.c
@@ -1,40 +1,44 @@
 /****************************************************
-* udpsg.c - UDP Server Multi Plattform
-* Stand 14.12.2023 - (C)Joembedded.de
+* udpsg.c - UDP Server Multi Plattform - (C)Joembedded.de
 *
-* Aus Performance-Gruenden wird nicht jedes UDP Paket einzeln verarbeitet,
-* sondern es wird versucht bis zu MAX_CLIENTS Pakete auf einmal zu lesen.
-* Dadurch kann ein nachfolgender libCurl-Aufruf das 'curl_multi_init'
-* Feature genutzt werden ( https://curl.se/ -> libcurl ). Eine schnellere
-* Loesung waere ein einzelner libCurl-Aufruf mit den Daten als Array.
-* Das wuerde aber das aufgerufene Script komplizierter machen. Daher lieber
-* die einfache Version mit 'curl_multi'.
-* 
+* For performance reasons, each UDP packet is not processed individually,
+* Instead, up to MAX_CLIENTS packets are read at once.
+* This allows a subsequent libCurl call to use the 'curl_multi_init'
+* feature ( https://curl.se/ -> libcurl ). A faster
+* solution would be a single libCurl call with the data as an array.
+* But this would make the called script more complicated. Therefore
+* the simple version with 'curl_multi'.
 *
-* Der Server kann unter Windows MS VS und Linux GCC uebersetzt werden.
 *
-* Windows: Fuer VS wird libcurl.dll benoetigt:
-* Installieren via vcpk (siehe https://github.com/curl/curl/blob/master/docs/INSTALL.md#building-using-vcpkg ):
+* UPSServerGeneral works with Windows (MS VS) ans Linux (GCC).
+*
+* Windows: For MS VS 'libcurl.dll' is required:
+* Install via vcpk (see https://github.com/curl/curl/blob/master/docs/INSTALL.md#building-using-vcpkg ):
 *   git clone https://github.com/Microsoft/vcpkg.git
 *   cd vcpkg
 *   bootstrap-vcpkg.sh
 *   vcpkg integrate install
 *   vcpkg install curl[tool]
-* Dauert ca. 5 min, dann kann libcurl wie gewohnt verwendet werden (exakt identisch zu gcc! Keine weiteren include oder libs noetig!)
+* Takes about 5 minutes, then libcurl can be used as usual (exactly identical
+* to gcc! No further include or libs necessary!)
 *
 * Linux:
 *   apt install libcurl4-openssl-dev -y
-* Dann einfach per 'gcc -lcurl' dazulinken
+* Link via 'gcc -lcurl'
 *
 * Dir: cd /var/www/vhosts/joembedded.eu/c
-* Uebersetzen als: gcc udpsg.c -o g.out
-* Test: nc  -u joembedded.eu 5288 -v -v
+* Compile: gcc udpsg.c -o g.out
 *
-*
-* Test with: nc -u localhost 5288 -v -v
-*            nc  -u joembedded.eu 5288 -v -v
+* Test with e.g:
+*    nc -u localhost 5288 -v
+*    nc -u joembedded.eu 5288 -v
+* For Tests run manually on console (set DEBUG 1),
+* for productin (mainly on LINUX) install as Service.
 *
 ****************************************************/
+
+#define VERSION  "V1.00 15.12.2023"
+#define DEBUG	1 // 1 for Output
 
 #ifdef _MSC_VER	// MS VC
 #define _CRT_SECURE_NO_WARNINGS
@@ -63,23 +67,21 @@ typedef int SOCKET;
 
 #include "curl/curl.h"
 
-#define PORT     5288	// UDP Listen Port
+//-----------------------------------------
+#define UDP_PORT     5288	// UDP Listen/Send Port
 #define CALLSCRIPT "http://localhost/wrk/udplog/payload_minimal.php?p=" // Hex-Payload will be added
+#define CALLSCRIPT_MAXRUN_MS	1500 // Recommended: <2 sec
+#define MAX_CLIENTS	10		// Maximum Number to UPD similar
+//-----------------------------------------
 
-#define URL_MAXLEN 128	// Chars
-#define RX_BUFLEN 1024  // Binary
-#define TX_BUFLEN 2048  // Chars
+#define URL_MAXLEN 160	// Chars
+#define RX_BUFLEN 1024  // Binary - UDP pakets are <= 1k by default
+#define TX_BUFLEN 2048  // Chars - 1k in chars
 
-#define IDLE_TIME 10 // Idle-Timeout 10 sec
+#define IDLE_TIME 10 // Server Loop Idle-Timeout 10 sec
+SOCKET sockfd = (SOCKET)0;	// Server, local Port
 
-
-SOCKET sockfd = (SOCKET)0;	// Server, localer Port
-
-long concnt = 0;// Counts Connections
-
-#define MAX_CLIENTS	10	// Maximum Number to receive, siehe oeben
 typedef struct {
-	long conid;	// Connection ID
 	struct sockaddr_in client_socket;	// Source
 	char rx_buffer[RX_BUFLEN + 1];  // OPt. String with 0-Terminator (Binary)
 	int rcv_len; // Receives len
@@ -88,7 +90,6 @@ typedef struct {
 } CLIENT;
 
 CLIENT clients[MAX_CLIENTS];
-
 int client_sock_len = sizeof(struct sockaddr_in);  //len is value/result 
 
 // Init Socket - Return 0 if OK, Errors: All != 0
@@ -98,24 +99,20 @@ int init_udp_server_socket(void) {
 	memset(&server_socket, 0, sizeof(server_socket));
 	server_socket.sin_family = AF_INET;
 	server_socket.sin_addr.s_addr = INADDR_ANY;
-	server_socket.sin_port = htons(PORT); // host2network
+	server_socket.sin_port = htons(UDP_PORT); // host2network
 
 #ifdef _MSC_VER	// MS VS only
 	WSADATA wsa;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return WSAGetLastError();  // use V2.2
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET) return WSAGetLastError();
 	if (bind(sockfd, (struct sockaddr*)&server_socket, sizeof(server_socket)) == SOCKET_ERROR) return WSAGetLastError();
-
-	// If iMode = 0, blocking is enabled; 
-	// If iMode != 0, non-blocking mode is enabled.
-	u_long iMode = 1;
+	u_long iMode = 1; // If iMode != 0, non-blocking mode is enabled.
 	int res = ioctlsocket(sockfd, FIONBIO, &iMode);
 	if (res != NO_ERROR) return WSAGetLastError();
 
 #else // GCC
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) return -1; // -1 Invalid (-2: Options)
 	if (bind(sockfd, (const struct sockaddr*)&server_socket, sizeof(server_socket)) < 0) return -3; // -3 Bind
-
 	if (fcntl(sockfd, F_SETFL, O_NONBLOCK) < 0) return -4; // Make it non-Blocking
 #endif
 	return 0;
@@ -131,30 +128,20 @@ int receive_from_udp_socket(int idx) {
 	int rcv_len = recvfrom(sockfd, pcli->rx_buffer, RX_BUFLEN, 0, (struct sockaddr*)&pcli->client_socket, &client_sock_len);
 	if (rcv_len == SOCKET_ERROR) {
 		int lastError = WSAGetLastError();
-		if (lastError == WSAETIMEDOUT) {
-			//printf("(Timeout)\n");
-			return 0;
-		}
-		if (lastError == WSAEWOULDBLOCK) {
-			//printf("(NoData)\n");
-			return 0;
-		}
-		if (lastError == WSAECONNRESET) {
-			//printf("((SocketReset(10054))\n"); // only TCP?
-			return 0;
-		}
+		if (lastError == WSAETIMEDOUT) 	return 0; //printf("(Timeout)\n");
+		if (lastError == WSAEWOULDBLOCK) return 0; //printf("(NoData)\n");
+		if (lastError == WSAECONNRESET) return 0; //printf("((SocketReset(10054))\n"); // only TCP?
 		if (lastError > 0) lastError = -lastError;
 		return lastError;
 	}
 #else // GCC
 	int rcv_len = recvfrom(sockfd, pcli->rx_buffer, RX_BUFLEN, 0, (struct sockaddr*)&pcli->client_socket, &client_sock_len);
-	if (rcv_len == -1) { // Timeout
-		//printf("(Timeout)\n");
-		return 0;
-	}
-	else if (rcv_len < 0) {
-		return rcv_len;
-	}
+	if (rcv_len == -1) return 0; //printf("(Timeout)\n");
+	else if (rcv_len < 0) return rcv_len;
+	
+#endif
+#if DEBUG
+	printf("Received %d Bytes from %s:%d\n", rcv_len,inet_ntoa(pcli->client_socket.sin_addr), ntohs(pcli->client_socket.sin_port));
 #endif
 	pcli->rx_buffer[rcv_len] = 0; // Important if Strings used!
 	pcli->rcv_len = rcv_len;
@@ -214,18 +201,12 @@ void close_udp_server_socket(void) {
 #endif
 }
 
-// --CURL Handler--
+// --CURL Handlers--
 static size_t curl_write_cb(char* data, size_t n, size_t len, void* userp)
 {
 	size_t realsize = n * len; // n: 1.d.R. 1
-
 	CLIENT* pcli = (CLIENT*)userp;
-	printf("WC_PCLI: %lx, id:%ld\n", (long)pcli, pcli->conid);
-
-	
 	int hlen = pcli->tx_len;
-	printf("Data for URL[%ld]:%d Bytes\n", pcli->conid,(int)realsize); 
-
 	size_t maxcopy = TX_BUFLEN - hlen;
 	if (maxcopy) { // limit to Maximum. Ignore Ovderdue
 		if (realsize <= maxcopy) maxcopy = realsize;
@@ -234,7 +215,6 @@ static size_t curl_write_cb(char* data, size_t n, size_t len, void* userp)
 		pcli->tx_replybuf[hlen] = 0;
 		pcli->tx_len = hlen;
 	}
-
 	return realsize;
 }
 
@@ -246,55 +226,57 @@ int run_curl(int anz) {
 
 	curl_global_init(CURL_GLOBAL_ALL);
 	cm = curl_multi_init();
-
 	/* Limit the amount of simultaneous connections curl should allow: */
 	curl_multi_setopt(cm, CURLMOPT_MAXCONNECTS, (long)anz);
-
 	for (int ridx = 0; ridx < anz; ridx++) {
 		CLIENT* pcli = &clients[ridx];
 		// Build URL (GET)
-		char* pc = url + sprintf(url,"%s", CALLSCRIPT);
+		char* pc = url + sprintf(url, "%s", CALLSCRIPT);
 		for (int di = 0; di < pcli->rcv_len; di++) {
 			uint8_t c = (pcli->rx_buffer[di]) & 255;
-			pc += sprintf(pc,"%02X",c);
+			pc += sprintf(pc, "%02X", c);
 		}
-		pcli->tx_len = 0;
-		pcli->conid = concnt++;	// Unique ID
-		printf("Add URL[%ld](Idx:%d):'%s'\n", pcli->conid, ridx, url); // Control
+		pcli->tx_len = 0;	// No Reply
+		pcli->tx_replybuf[0] = 0;	
+#if DEBUG
+		printf("Add URL(Idx:%d):'%s'\n", ridx, url); // Control
+#endif
 		CURL* eh = curl_easy_init();
 		curl_easy_setopt(eh, CURLOPT_URL, url);
 		curl_easy_setopt(eh, CURLOPT_WRITEFUNCTION, curl_write_cb);
-		printf("PCLI: %lx, id:%ld\n", (long)pcli, pcli->conid);
+		// Security Simplifications
+		curl_easy_setopt(eh, CURLOPT_FOLLOWLOCATION, 1L); // Follow Redirections
+		curl_easy_setopt(eh, CURLOPT_SSL_VERIFYPEER, 0L); // No Verify
+		curl_easy_setopt(eh, CURLOPT_SSL_VERIFYHOST, 0L); // No Verify
+		curl_easy_setopt(eh, CURLOPT_TIMEOUT_MS, CALLSCRIPT_MAXRUN_MS); // Script must complete fast
 		curl_easy_setopt(eh, CURLOPT_WRITEDATA, pcli);
 		curl_easy_setopt(eh, CURLOPT_PRIVATE, pcli);
 		curl_multi_add_handle(cm, eh);
 	}
 
-	int left = anz;
-	int msgs_left = -1;
-	do {
-		int still_alive = 1;
-		curl_multi_perform(cm, &still_alive);
-
+	for(;;){
+		int cactive = 0; // Connections active
+		curl_multi_perform(cm, &cactive);
+		int msgs_left = -1; // Dummy
 		while ((msg = curl_multi_info_read(cm, &msgs_left))) {
-			if (msg->msg == CURLMSG_DONE) {
+			if (msg->msg == CURLMSG_DONE) { // Currently only CURLMSG_DONE defined by Curl
 				CURL* e = msg->easy_handle;
 				CLIENT* pcli;
-
 				curl_easy_getinfo(e, CURLINFO_PRIVATE, (void*)&pcli);
-				printf("Reply[%ld]: '%s' ", pcli->conid,pcli->tx_replybuf);
-				printf("Result: %d:%s\n", msg->data.result, curl_easy_strerror(msg->data.result));
-
+				int cres = msg->data.result; // 0: OK, e.g. 28:Timeout
+#if DEBUG
+				int idx = (int)(pcli - &clients[0]);
+				printf("Reply(Ind.%d): '%s'[%d] ", idx, pcli->tx_replybuf, pcli->tx_len);
+				printf("Result: %d:%s\n",cres , curl_easy_strerror(cres));
+				printf("Dest: %s:%d\n", inet_ntoa(pcli->client_socket.sin_addr), ntohs(pcli->client_socket.sin_port));
+#endif
 				curl_multi_remove_handle(cm, e);
 				curl_easy_cleanup(e);
-				left--;
-			}
-			else {
-				printf("WARNING: CURLMsg(%d)\n", msg->msg);
-			}
+			} // else ignore
 		}
-		if (left) curl_multi_wait(cm, NULL, 0, 10, NULL); // Wait 10 msec
-	} while (left);
+		if (cactive) curl_multi_wait(cm, NULL, 0, 10, NULL); // Wait 10 msec
+		else break;
+	} 
 
 	curl_multi_cleanup(cm);
 	curl_global_cleanup();
@@ -305,17 +287,16 @@ int run_curl(int anz) {
 int udp_server_loop(void) {
 	int wcnt = 0;
 	while (1) {
+#if DEBUG
 		printf(" Wait(%d) ", wcnt++);
 		fflush(stdout);
-
+#endif
 		int res = wait_for_udp_data();
 		if (res < 0) {
 			printf("ERROR: 'select()' failed (%d)\n", res);
 			return res;
 		}
-		else if (!res) { // Timeout
-			continue;
-		}
+		else if (!res) continue; // Timeout
 
 		int cidx;
 		for (cidx = 0; cidx < MAX_CLIENTS; cidx++) {
@@ -325,56 +306,31 @@ int udp_server_loop(void) {
 				printf("ERROR: ReceiveFrom UDP Server Socket Index[%d] failed (%d)\n", cidx, res);
 				return res;
 			}
-			else if (!res) { // Nothing
-				//printf("WARNING: ReceiveFrom UDP Server Socket Index[%d] Nothing (%d)\n", cidx, res);
-				break;
-			}
-			int rcv_len = res;
+			else if (!res) break; // Nothing (more) received
 		}
+		/*res =*/ run_curl(cidx); // cidx: Nr of packets received
 
-		printf("Anz. Pakete emmpfangen: %d\n", cidx);
 
-		res = run_curl(cidx);
 
-		/*
-		int ridx;	// Reply-Index
-		for (ridx = 0; ridx < cidx; ridx++) {
-			CLIENT* pcli = &clients[ridx];
+		/* Optional Delay for Tests */
+		printf("*** Warte ***\n");
+#ifdef _MSC_VER	// MS VS only
+		Sleep(8000);
+#else // GCC
+		sleep(5);
+#endif
 
-			//print details of the client/peer and the data received
-			printf("Rec.Idx[%d] from %s:%d: ", ridx, inet_ntoa(pcli->client_socket.sin_addr), ntohs(pcli->client_socket.sin_port));
-			printf("Data[%d]: ", pcli->rcv_len);
-			for (int di = 0; di < pcli->rcv_len; di++) {
-				uint8_t c = (pcli->rx_buffer[di]) & 255;
-				printf("%02X",c);
-			}
-			printf("\n");
 
-			// Minimum Reply
-			time_t ct = time(NULL);
-			uint32_t unixsec = (uint32_t)ct;
-			sprintf(tx_replybuf, "FF%08X", unixsec);	// FF: plus Time (UTC.u32)
-			int tx_len = (int)strlen(tx_replybuf);
-
-			//now reply the client
-			res = send_reply_to_udp_client(ridx, tx_len);
-			if (res) {
-				printf("ERROR: SendTo UDP Client Socket Index[%d] failed (%d)\n", ridx, res);
-				break;
-			}
-
-		}
-		*/
 	}
 
 }
 
 // --- MAIN ---
 int main() {
-
-	printf("--- UDP Server General " __DATE__ " " __TIME__ "\n");
+	printf("--- UDPServerGeneral %s - (C) Joembedded\n", VERSION);
+	printf("--- Script: '%s'\n", CALLSCRIPT);
 	if (strlen(CALLSCRIPT) > (URL_MAXLEN - 1)) {
-		printf("ERROR: CALLSCRIP Len!\n");
+		printf("ERROR: CALLSCRIPT Len!\n");
 		return -1;
 	}
 	int res = init_udp_server_socket();
@@ -383,10 +339,8 @@ int main() {
 		return -1;
 	}
 	else {
-
-		printf("---Wait on port %d---\n", PORT);
+		printf("--- Wait on UDP Port %d...\n", UDP_PORT);
 		udp_server_loop();
-
 	}
 	close_udp_server_socket();
 	return 0;
